b9ee113ae63e37b7c564a49829c6afa9f5f992c1
master <master@masterdeMacBook-Pro.local>
2019-05-08 13:11:56 +0800

    2.2.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rsj217/service
+
+go 1.12
--- http.go
+++ http.go
@@ -6,5 +6,5 @@ import "fmt"
 func HandleReq(){
 
     fmt.Println("api - http.go Handle Request")
-    fmt.Println("v2.1.0")
+    fmt.Println("v2.2.0")
 }
